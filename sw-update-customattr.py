7d674e075180e7ceda1c5b8e1939395edf288247
David Thiele <jdthiele@gmail.com>
2019-08-29 16:41:53 -0500

    created the customattr script

--- sw-update-customattr.py
+++ sw-update-customattr.py
@@ -12,7 +12,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # define some help menu strings
 
-help_header = """This script will help automate the process of getting a hosts listing from SolarWinds
+help_header = """This script will help automate the process of updating custom attribute values in SolarWinds.
 
 Refer to https://github.com/jdthiele/orionsdk-python_scripts for detailed help."""
 
@@ -21,12 +21,14 @@ parser = argparse.ArgumentParser(description=help_header)
 parser.add_argument("-s", "--npm_server", help="Provide the name of the SolarWinds server", required=True)
 parser.add_argument("-u", "--user", help="Provide the user to connect to the OrionSDK as", required=True)
 parser.add_argument("-p", "--password", help="Provide the password for the given user, or omit to be prompted")
-parser.add_argument("-a", "--attribute", help="Provide the attribute to update", required=True)
+parser.add_argument("-n", "--node", help="Provide the node you want to update the machinetype property on", required=True)
+parser.add_argument("-c", "--customproperty", help="Provide the attribute to update", required=True)
 parser.add_argument("-v", "--value", help="Provide the value to set for the given attribute", required=True)
 args = parser.parse_args()
 npm_server = args.npm_server
 user = args.user
-custom_attr = args.attribute
+node = args.node
+custom_property = args.customproperty
 custom_val = args.value
 
 # ask for a password if not provided in args
@@ -38,24 +40,48 @@ else:
 # load the swis client and login to the NPM server
 swis = orionsdk.SwisClient(npm_server, user, password, verify=False)
 
-# define the query to run via the client
-query1 = """
-select n.Caption
-from
-  Orion.Nodes n
-JOIN
-  Orion.NodesCustomProperties c
-  ON n.nodeid = c.nodeid
-WHERE c.ManagingTeam = 'UNIX'
-ORDER BY n.caption;"
-"""
+# run the query to get the node URI and save the output
+uri_query = 'SELECT Uri from Orion.Nodes where Caption=\'' + node + '\''
+results = swis.query(uri_query)
+if results['results'] == []:
+    print('Did not find a host with this name. Exitting.')
+    sys.exit(1)
+nodeuri = results['results'][0]['Uri']
+uri = nodeuri + '/CustomProperties'
 
-# run the query and save the output
-queryresults = swis.query(query1)
+# check of the custom property exists and what the current value is
+obj = swis.read(uri)
+current_val = ""
+if custom_property in obj:
+  current_val = obj[custom_property]
+else:
+  print("The custom property " + custom_property + " does not exist. Exitting.")
+  sys.exit(1)
 
-# print the output in a human readable format
-for row in queryresults['results']:
-    print ("{Caption}".format(**row))
+# check if the value is valid
+query = 'select Value from Orion.CustomPropertyValues where Field=\'' + custom_property + '\''
+results = swis.query(query)
+avail_values = []
+for dict in results["results"]:
+  v = dict["Value"]
+  avail_values.append(v)
+
+if custom_val not in avail_values:
+  print("The value you provided is not in the list of existing values. Please use one of: ")
+  print(avail_values)
+  sys.exit(2)
+
+# if the given value is different than current, update it
+if current_val == custom_val:
+  print("The value " + custom_val + " is already set")
+  sys.exit(0)
+else:
+  props = {custom_property: custom_val}
+  swis.update(uri, **props)
+
+# print the new value if changed
+obj = swis.read(uri)
+print("Changed " + custom_property + " to: " + custom_val)
 
 # Exit with a code of 0 indicating all went well
 sys.exit(0)
